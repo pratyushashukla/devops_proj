6096c34f0b1a492396b0f110c55da8db97f6ce47
Pratyusha Shukla <34051258+pratyushashukla@users.noreply.github.com>
2024-08-16 10:04:33 -0400

    update 2 test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React along with round testing!
+          Learn React along with round testing 2!
         </a>
       </header>
     </div>
